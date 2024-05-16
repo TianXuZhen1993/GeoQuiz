d1904ef51af189890e5c17f7433b2d845ddf2d79
XzTian【田许珍】 <97434@cif01.chailease.com.tw>
2024-05-16 19:03:13 +0800

    spUtils

---
+++ library_base/src/main/java/com/example/library_base/utils/SPUtils.kt
@@ -0,0 +1,31 @@
+package com.example.library_base.utils
+
+import android.content.SharedPreferences
+
+/**
+ *
+ *
+ * @author TXZ
+ * @version 1.0
+ * created by 2024/5/16 18:26
+ */
+object SPUtils {
+    //项目APP的基础设置
+    private const val APP_CONFIG = "APP_CONFIG"
+
+    private lateinit var sp: SharedPreferences
+
+    /**
+     * 返回SharedPreferences实例
+     *
+     * @return
+     */
+    fun getInstance(): SharedPreferences {
+        return sp
+    }
+
+    fun put(key: String, value: String) {
+        val edit = sp.edit()
+        edit.apply()
+    }
+}
